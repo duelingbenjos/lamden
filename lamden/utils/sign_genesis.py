9b6a0277d9242706dcadf9d320777e09045ecb67
duckfever <benjor@gmail.com>
2023-12-10 00:38:39 +0000

    improve cwd

--- lamden/utils/sign_genesis.py
+++ lamden/utils/sign_genesis.py
@@ -1,3 +1,4 @@
+import os
 from argparse import ArgumentParser
 import json
 from lamden.crypto.block_validator import GENESIS_BLOCK_NUMBER, GENESIS_HLC_TIMESTAMP, GENESIS_PREVIOUS_HASH
@@ -6,9 +7,11 @@ from lamden.crypto.wallet import Wallet
 from contracting.db.encoder import encode, decode
 
 
-def main(unsigned_genesis_block_path: dict, output_path: str, sk: str):
+def main(source_genesis: dict, output_path: str, sk: str):
+    cwd = os.getcwd()
+    source_genesis_path = os.path.join(cwd, source_genesis)
 
-    with open(unsigned_genesis_block_path) as f:
+    with open(source_genesis_path) as f:
         unsigned_genesis_block = json.load(f)
 
     genesis_block = {
@@ -38,4 +41,4 @@ if __name__ == '__main__':
     parser.add_argument('-o', type=str, required=True)
     args = parser.parse_args()
 
-    main(unsigned_genesis_block_path=args.g, output_path=args.o, sk=args.sk)
+    main(source_genesis=args.g, output_path=args.o, sk=args.sk)
